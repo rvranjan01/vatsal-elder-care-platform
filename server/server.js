1a54869ad40f8f1e1100fb03dd8056bce4bfe2d1
rvranjan01 <vermaranjan2001@gmail.com>
2026-01-31 17:43:10 +0530

    Initial commit for Vatsal project

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+connectDB();
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Vatsal Backend Running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
