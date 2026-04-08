7f4a2033b67491adbeda8cf619d4b7d626401f2a
rvranjan <vermaranjan2001@gmail.com>
2026-04-08 19:41:27 +0530

    Frontend api.js & socket.js changes

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,10 +1,5 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL =
-  process.env.REACT_APP_SOCKET_URL ||
-  window.location.origin.replace(/^http/, "ws");
-
-// Create and export a singleton socket instance
 const socket = io(
   process.env.REACT_APP_SOCKET_ENDPOINT || "http://localhost:5000",
   {
