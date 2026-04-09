afbe5cfee33da84c2c661c58eee4fa872585457d
rvranjan <vermaranjan2001@gmail.com>
2026-04-09 21:09:38 +0530

    Frontend socket.js changes

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,10 +1,26 @@
+// import { io } from "socket.io-client";
+
+// const socket = io(
+//   process.env.REACT_APP_SOCKET_ENDPOINT || "http://localhost:5000",
+//   {
+//     transports: ["websocket", "polling"],
+//   },
+// );
+
+// export default socket;
+
+
 import { io } from "socket.io-client";
 
-const socket = io(
-  process.env.REACT_APP_SOCKET_ENDPOINT || "http://localhost:5000",
-  {
-    transports: ["websocket", "polling"],
-  },
-);
+const SOCKET_URL =
+  process.env.REACT_APP_SOCKET_ENDPOINT || "http://localhost:5000";
+
+const socket = io(SOCKET_URL, {
+  withCredentials: true,
+  transports: ["polling", "websocket"],
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+});
 
 export default socket;
